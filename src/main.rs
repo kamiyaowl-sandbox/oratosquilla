acbce91ac8f44c840be2f3fffa5c011acfa0dfe9
K <kamiyaowl@gmail.com>
2019-11-03 16:15:21 +0900

    なんとなく書き始めた

---
+++ src/main.rs
@@ -0,0 +1,123 @@
+mod maze {
+    pub const MAZE_WIDTH: usize = 32;
+    pub const MAZE_HEIGHT: usize = 32;
+
+    #[derive(Copy,Clone,Debug)]
+    pub struct Point {
+        x: usize,
+        y: usize,
+    }
+    /// 各区画単位の管理情報
+    #[derive(Copy, Clone, Debug)]
+    pub struct Cell {
+        // 区画上方向の壁有無
+        pub up_wall: Option<bool>,
+        // 区画右方向の壁有無
+        pub right_wall: Option<bool>,
+        /// ここまでの到達に必要な手数
+        pub cost: Option<usize>,
+    }
+
+    impl Default for Cell {
+        fn default() -> Self {
+            Self {
+                up_wall: None,
+                right_wall: None,
+                cost: None,
+            }
+        }
+    }
+
+    /// 実機から迷路情報の更新に使う情報
+    #[derive(Debug)]
+    pub struct UpdateInfo {
+        pub p: Point,
+        pub up: Option<bool>,
+        pub down: Option<bool>,
+        pub left: Option<bool>,
+        pub right: Option<bool>,
+    }
+
+    use std::ops::Sub;
+    impl Sub for Point {
+        type Output = usize;
+        /// ビルドオプション指定がなければ、チェビシフ距離を返します
+        fn sub(self, other: Point) -> usize {
+            use std::cmp;
+
+            let dx = cmp::max(self.x, other.x) - cmp::min(self.x, other.x);
+            let dy = cmp::max(self.y, other.y) - cmp::min(self.y, other.y);
+
+            if cfg!(distance_method = "manhattan") {
+                dx + dy
+            } else {
+                cmp::max(dx, dy)
+            }
+        }
+    }
+
+    /// 迷路管理の親
+    #[derive(Debug)]
+    pub struct Maze {
+        pub cells: [[Cell; MAZE_WIDTH]; MAZE_HEIGHT],
+        pub previous_cell: Option<Point>,
+        pub start: Point,
+        pub goal: Point,
+    }
+
+    impl Default for Maze {
+        fn default() -> Self {
+            Self {
+                cells: [[Cell::default(); MAZE_WIDTH]; MAZE_HEIGHT],
+                previous_cell: None,
+                start: Point { x: 0, y: 0 },
+                goal: Point { x: 0, y: 0 },
+            }
+        }
+    }
+
+    impl Maze {
+        pub fn init(&mut self, goal: Point) {
+            self.cells = [[Cell::default(); MAZE_WIDTH]; MAZE_HEIGHT];
+            self.start = Point { x: 0, y: 0 };
+            self.goal = goal;
+            // 上端、右端の壁初期化
+            for j in 0..MAZE_HEIGHT {
+                self.cells[j][MAZE_WIDTH - 1].right_wall = Some(true);
+            } 
+            for i in 0..MAZE_WIDTH {
+                self.cells[MAZE_HEIGHT - 1][i].up_wall = Some(true);
+            }
+        }
+        /// 壁情報を更新する
+        pub fn update(&mut self, info: &UpdateInfo) {
+            debug_assert!(info.p.x < MAZE_WIDTH);
+            debug_assert!(info.p.y < MAZE_HEIGHT);
+            // 壁情報の更新
+            if let Some(up_wall) = info.up {
+                self.cells[info.p.y][info.p.x].up_wall = Some(up_wall);
+            }
+            if let Some(right_wall) = info.right {
+                self.cells[info.p.y][info.p.x].right_wall = Some(right_wall);
+            }
+            // 下、左は隣のセル情報に格納されている
+            if info.p.y > 0 {
+                if let Some(down_wall) = info.down {
+                    self.cells[info.p.y - 1][info.p.x].up_wall = Some(down_wall);
+                }
+            }
+            if info.p.x > 0 {
+                if let Some(left_wall) = info.left {
+                    self.cells[info.p.y][info.p.x - 1].right_wall = Some(left_wall);
+                }
+            }
+            // 最終更新履歴を残す
+            self.previous_cell = Some(info.p);
+        }
+    }
+}
+
+fn main() {
+    use maze::*;
+    println!("Hello, world! {}", MAZE_WIDTH);
+}
